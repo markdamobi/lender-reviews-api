00e5067548758d2a794507ef82062b688ad5c9bd
Mark Donald Amobi <markdamobi@gmail.com>
2019-04-12 21:35:59 -0500

    handle routing error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,17 @@
 class ApplicationController < ActionController::API
-  rescue_from Exceptions::BaseError, :with => :handle_error
+  rescue_from ActionController::RoutingError, with: :handle_routing_error
+  rescue_from Exceptions::BaseError, with: :handle_error
 
   def handle_error(error)
     render json: { error: error.to_h }, status: error.status
+  end
+
+  def handle_routing_error(error)
+    logger.error 'Routing error occurred'
+    render json: { error: { message: error.message } }, status: 404 
+  end
+
+  def catch_routing_error
+    raise ActionController::RoutingError.new("the endpoint #{params[:path]} is not available.")
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
   get "reviews", to: "reviews#index"
+  match "*path", to: "application#catch_routing_error", via: :all
 end
